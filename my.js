119f914741b24a6e0b15604cff7380fb1ef375fa
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-23 15:52:23 +0100

    ulteriore implementazione programma

--- my.js
+++ my.js
@@ -1,9 +1,18 @@
 let numeroUno= 8;
-let numeroDUE= 2;
-let result= numeroUno % numeroDUE === 0;
+let numeroTre= 3;
+let numeroCinque= 5;
+let result= numeroUno % numeroTre === 0;
 console.log(result);
 
 
 for (let i=0 ; i < 101 ; i++){
+    
+    if(i % numeroTre === 0){
+        console.log('fizz')
+    }else if(i % numeroCinque === 0){
+        console.log('buzz')
+
+    }
+    console.log(i)
     
 }
