a94d4c72f716795a915e69b5f0c7cccd8177e849
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-23 17:27:13 +0100

    ulteriori mofiche al programma add: fizzbuzz

--- my.js
+++ my.js
@@ -1,21 +1,17 @@
-let numeroUno= 8;
-let numeroTre= 3;
-let numeroCinque= 5;
-let result= numeroUno % numeroTre === 0;
-console.log(result);
-
 
 for (let i=1 ; i < 101 ; i++){
-
-    if(i % numeroTre === 0){
-        console.log('fizz')
     
-    }else if(i % numeroCinque === 0){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log('fizzbuzz')
+    }else if(i % 3 == 0){
+        console.log('fizz')
+        
+    }else if(i % 5 == 0){
         console.log('buzz')
-
     }
+
     else{
         console.log(i)
     }
-    
+      
 }
